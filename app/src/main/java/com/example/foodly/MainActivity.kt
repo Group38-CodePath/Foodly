7d2e0a24ab8c1f9eb5ea56000752a71cb965c40e
Ella <emmanuellaumoye18@gmail.com>
2022-04-22 22:29:49 -0400

    keysadded

--- app/src/main/java/com/example/foodly/MainActivity.kt
+++ app/src/main/java/com/example/foodly/MainActivity.kt
@@ -3,10 +3,38 @@ package com.example.foodly
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.foodly.fragment.FavoriteFragment
+import com.example.foodly.fragment.HomeFragment
+import com.example.foodly.fragment.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemReselectedListener { item ->
+            var fragmentToShow: Fragment? = null
+
+            when (item.itemId) {
+                R.id.action_home -> {
+                    //TODO: Navigate to homescreen
+                    fragmentToShow = HomeFragment()
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                }
+                R.id.favorite_home -> {
+                    fragmentToShow = FavoriteFragment()
+                    Toast.makeText(this, "Favorite", Toast.LENGTH_SHORT).show()
+                }
+                R.id.profile_home -> {
+                    fragmentToShow = ProfileFragment()
+                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/foodly/fragment/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.example.foodly.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodly.Post
+import com.example.foodly.R
+import com.example.foodly.RecipeAdapter
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    lateinit var rvRecipeHome: RecyclerView
+    lateinit var adapter: RecipeAdapter
+    var allPosts: MutableList<Post> = mutableListOf()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //sETUP VIEWS HERE AND ONCLICK LISTNERES
+        rvRecipeHome = view.findViewById(R.id.rvRecipeHome)
+
+    }
+}
