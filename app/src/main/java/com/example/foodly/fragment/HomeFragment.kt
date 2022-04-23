6ae1e3af4ea3cc16217ecaf987bc67ac54775eb4
yuniskhamis <yuniskhamis3@gmail.com>
2022-04-23 19:00:36 -0400

    Retrieve meals from network

--- app/src/main/java/com/example/foodly/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/foodly/fragment/HomeFragment.kt
@@ -1,25 +1,31 @@
 package com.example.foodly.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.example.foodly.Post
 import com.example.foodly.R
 import com.example.foodly.RecipeAdapter
+import com.example.foodly.model.Meal
+import okhttp3.Headers
+import org.json.JSONException
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+private const val TAG = "HomeFragment"
+
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     lateinit var rvRecipeHome: RecyclerView
-    lateinit var adapter: RecipeAdapter
+    lateinit var searchView: SearchView
+    private val meals = mutableListOf<Meal>()
+
     var allPosts: MutableList<Post> = mutableListOf()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,8 +37,59 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //sETUP VIEWS HERE AND ONCLICK LISTNERES
+        val recipeAdapter = RecipeAdapter(requireContext(),meals)
         rvRecipeHome = view.findViewById(R.id.rvRecipeHome)
 
+        rvRecipeHome.adapter = recipeAdapter
+
+        rvRecipeHome.layoutManager = LinearLayoutManager(requireContext())
+
+        searchView = view.findViewById(R.id.searchView)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit( query: String): Boolean {
+
+                val client = AsyncHttpClient()
+                val MEAL_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=${query}"
+                client.get(MEAL_URL, object: JsonHttpResponseHandler() {
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Headers?,
+                        response: String?,
+                        throwable: Throwable?
+                    ) {
+                        Log.e(TAG,"onFailure: $statusCode")
+                    }
+
+                    override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                        Log.i(TAG,"onSuccess: JSON data $json")
+
+                        try {
+                            val mealJsonArray = json.jsonObject.getJSONArray("meals")
+                            meals.clear()
+                            meals.addAll(Meal.fromJsonArray(mealJsonArray))
+                            recipeAdapter.notifyDataSetChanged()
+                            Log.i(TAG, "Meal list $meals")
+
+                          // TODO() Show toast if api returns empty list from search (ex. injnj)
+
+
+                        } catch(e: JSONException){
+                            Log.e(TAG,"Encountered exception $e")
+                        }
+                    }
+                })
+                return true
+            }
+            override fun onQueryTextChange(s: String?): Boolean {
+                return false
+            }
+
+        })
+
+
     }
+
+
 }
