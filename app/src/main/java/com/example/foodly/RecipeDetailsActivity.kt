58911d683fd3d0dee0de5a098a1012d1e4c2ac23
yuniskhamis <yuniskhamis3@gmail.com>
2022-04-24 17:05:23 -0400

    Created RecipeDetailsActivity.kt, set the data to the views, and initialize youtube

---
+++ app/src/main/java/com/example/foodly/RecipeDetailsActivity.kt
@@ -0,0 +1,38 @@
+package com.example.foodly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.foodly.databinding.ActivityRecipeDetailsBinding
+import com.example.foodly.model.Meal
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+
+class RecipeDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRecipeDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRecipeDetailsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val meal = intent.getParcelableExtra<Meal>(MEAL_EXTRA) as Meal
+
+        binding.tvMealTitle.text = meal.strMeal
+        binding.tvInstructions.text = meal.strInstructions
+        initializeYoutube()
+    }
+
+    private fun initializeYoutube() {
+        binding.player.addYouTubePlayerListener(object :
+            AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+               val meal = intent.getParcelableExtra<Meal>(MEAL_EXTRA) as Meal
+                val videoId = meal.strYoutube.substring(32,meal.strYoutube.length)
+                youTubePlayer.loadVideo(videoId,0f)
+                Toast.makeText(applicationContext,videoId,Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+}
