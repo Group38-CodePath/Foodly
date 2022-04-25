9a2e7e20ea9455cec2880bdd7f6c421d4b24eadd
yuniskhamis <yuniskhamis3@gmail.com>
2022-04-25 07:55:47 -0400

    Created Parse Class "RecipeFavorites", added appropriate getters and setters

---
+++ app/src/main/java/com/example/foodly/RecipeFavorites.kt
@@ -0,0 +1,49 @@
+package com.example.foodly
+
+import com.parse.ParseClassName
+import com.parse.ParseObject
+import com.parse.ParseUser
+
+@ParseClassName("RecipeFavorites")
+class RecipeFavorites : ParseObject() {
+
+    fun setRecipeId(mealId: Int?) {
+        put(KEY_MEAL_ID, mealId!!)
+    }
+
+    fun getRecipeId(): Int {
+        return getInt(KEY_MEAL_ID)
+    }
+
+    fun getImageUrl(): String? {
+        return getString(KEY_IMAGE_URL)
+    }
+
+    fun setImageUrl(imageUrl: String?) {
+        put(KEY_IMAGE_URL, imageUrl!!)
+    }
+
+    fun getTitle(): String? {
+        return getString(KEY_TITLE)
+    }
+
+    fun setTitle(title: String?) {
+        put(KEY_TITLE, title!!)
+    }
+
+    fun getUser(): ParseUser? {
+        return getParseUser(KEY_USER)
+    }
+
+    fun setUser(user: ParseUser) {
+        put(KEY_USER, user)
+    }
+
+
+    companion object {
+        const val KEY_USER  = "userId"
+        const val KEY_IMAGE_URL = "imageUrl"
+        const val KEY_TITLE = "title"
+        const val KEY_MEAL_ID = "mealId"
+    }
+}
