f13327b8bea933285cbda9ba1fd537a278c7f298
yuniskhamis <yuniskhamis3@gmail.com>
2022-04-25 09:32:17 -0400

    Add action to the action bar and respond to action buttons

--- app/src/main/java/com/example/foodly/MainActivity.kt
+++ app/src/main/java/com/example/foodly/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.foodly
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -9,6 +12,7 @@ import com.example.foodly.fragment.FavoriteFragment
 import com.example.foodly.fragment.HomeFragment
 import com.example.foodly.fragment.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.parse.ParseUser
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,5 +46,21 @@ class MainActivity : AppCompatActivity() {
         // Set default selection
         findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.logout -> {
+                // Handle more item (inside overflow menu) press
+                ParseUser.logOut()
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
