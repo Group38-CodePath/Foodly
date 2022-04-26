30245e3c1587a5abf8a05031b2132933c6df4e78
yuniskhamis <yuniskhamis3@gmail.com>
2022-04-25 20:09:44 -0400

    remove favorite on checkbox click

--- app/src/main/java/com/example/foodly/adapters/FavoriteRecipeAdapter.kt
+++ app/src/main/java/com/example/foodly/adapters/FavoriteRecipeAdapter.kt
@@ -1,4 +1,5 @@
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,9 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.foodly.R
 import com.example.foodly.RecipeFavorites
+import com.example.foodly.adapters.TAG
+import com.parse.ParseObject
+import com.parse.ParseQuery
 
 class FavoriteRecipeAdapter(val context: Context, val meals:MutableList<RecipeFavorites>): RecyclerView.Adapter<FavoriteRecipeAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -19,6 +23,33 @@ class FavoriteRecipeAdapter(val context: Context, val meals:MutableList<RecipeFa
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val post = meals.get(position)
+        holder.submitFavoriteButton.setOnClickListener {
+            val query = ParseQuery.getQuery<RecipeFavorites>("RecipeFavorites")
+            //query.whereEqualTo(KEY_USER, parseRecipe.getUser())
+            query.whereEqualTo(RecipeFavorites.KEY_MEAL_ID, meals[position].getRecipeId())
+            // The query will search for a ParseObject, given its objectId.
+            // When the query finishes running, it will invoke the GetCallback
+            // with either the object, or the exception thrown
+            query.getFirstInBackground { `object`, e ->
+                if (`object` != null) {
+                    Log.i(TAG, "Deleting $`object`")
+                    val query = ParseQuery.getQuery<ParseObject>("RecipeFavorites")
+                    query.getInBackground(
+                        `object`.objectId
+                    ) { `object`, e ->
+                        if (e == null) {
+                            // Otherwise, you can delete the entire ParseObject from the database
+                            `object`?.deleteInBackground()
+                        }
+                    }
+                } else if (e != null) {
+                    Log.e(TAG, "Error: $e")
+                }
+            }
+
+            meals.removeAt(position)
+            notifyDataSetChanged()
+        }
         holder.bind(post)
     }
 
@@ -55,7 +86,6 @@ class FavoriteRecipeAdapter(val context: Context, val meals:MutableList<RecipeFa
 
         fun bind(meal:RecipeFavorites) {
             mealName.text = meal.getTitle()
-
             Glide.with(itemView.context)
                 .load(meal.getImageUrl())
                 .apply(RequestOptions().override(600, 600))
